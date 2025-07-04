1c3ec217407d2ff6386cc960a0d6d5e5c02ac488
Rahul Prajapati <rp2628336@gmail.com>
2025-07-04 23:35:05 +0530

    backend added

--- frontend/src/services/Apis.jsx
+++ frontend/src/services/Apis.jsx
@@ -1,6 +1,7 @@
 // const BASE_URL = import.meta.env.VITE_BASE_URL;
-const BASE_URL = "";
+const BASE_URL = "https://swapskill-3546.onrender.com";
 // const BASE_URL = "http://localhost:5001";
+//https://swapskill-3546.onrender.com
 export const authEndpoits = {
   SIGNUP_API: `${BASE_URL}/api/v1/auth/signUp`,
   SIGNIN_API: `${BASE_URL}/api/v1/auth/login`,
